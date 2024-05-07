40d6ea22071e62ae170713f00b0d134f54b75566
judah <ygoldfed@gmail.com>
2024-05-07 16:18:08 -0400

    bugs

--- dataset_factory.py
+++ dataset_factory.py
@@ -256,12 +256,12 @@ def create_dataset(
                 if x.shape[0] == 1:
                     return x.repeat(3,1,1)
                 return x
-            transform = transforms.Compose([transforms.ToTensor(),transforms.Lambda(lambda x: f(x))])
+            transform = transforms.Compose([transforms.Lambda(lambda x: f(x))])
             generator = torch.Generator().manual_seed(42)
-            # full_dataset = Caltech256(**torch_kwargs,transform=transform)
+            full_dataset = Caltech256(**torch_kwargs,transform=transform)
             print(root)
-            full_dataset = torchvision.datasets.ImageFolder(root=root+"/256_ObjectCategories/",transform=transform)
-            print(full_dataset.classes)
+            #full_dataset = torchvision.datasets.ImageFolder(root=root+"/256_ObjectCategories/",transform=transform)
+            #print(full_dataset.classes)
             train_size = int(0.8 * len(full_dataset))
             test_size = len(full_dataset) - train_size
             train_dataset, test_dataset = torch.utils.data.random_split(full_dataset, [train_size, test_size],generator=generator)
