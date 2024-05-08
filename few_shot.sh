b284f4ac39e06e37e982df67347cbfcea2470c78
judah <ygoldfed@gmail.com>
2024-05-08 00:14:08 -0400

    few shot script

--- few_shot.sh
+++ few_shot.sh
@@ -17,7 +17,7 @@ CUDA_VISIBLE_DEVICES=4 python3 train.py /data/torch/caltech256/caltech256 --data
 CUDA_VISIBLE_DEVICES=4 python3 train.py /data/torch/caltech256/caltech256 --dataset torch/caltech256  --model=resnet50  --num-classes=257  --log-wandb --experiment "caltech256 1shot 2-1-all" --diffaug-dir=/data/puma_envs/control_augmented_images_caltech256_512  --variations 2 --repeats  --name "exp" --recipe "sgd-pretrain-fullaug"  --repeats 8 --variations 2 --diffaug-fewshot=1 
 
 
-CUDA_VISIBLE_DEVICES=4 python3 train.py /data/torch/caltech256/caltech256 --dataset torch/caltech256  --model=resnet50  --num-classes=257  --log-wandb --experiment "caltech256 1shot 2-1-all" --diffaug-dir=/data/puma_envs/control_augmented_images_caltech256_512  --variations 2 --repeats  --name "base" --recipe "sgd-pretrain-fullaug"  --repeats 24  --diffaug-fewshot=1 --classes 
+CUDA_VISIBLE_DEVICES=4 python3 train.py /data/torch/caltech256/caltech256 --dataset torch/caltech256  --model=resnet50  --num-classes=257  --log-wandb --experiment "caltech256 1shot 2-1-all" --diffaug-dir=/data/puma_envs/control_augmented_images_caltech256_512  --variations 2 --repeats  --name "base" --recipe "sgd-pretrain-fullaug"  --repeats 24  --diffaug-fewshot=1  
 
 
 
@@ -50,4 +50,4 @@ CUDA_VISIBLE_DEVICES=4 python3 train.py /data/torch/caltech256/caltech256 --data
 CUDA_VISIBLE_DEVICES=4 python3 train.py /data/torch/caltech256/caltech256 --dataset torch/caltech256  --model=resnet50  --num-classes=257  --log-wandb --experiment "caltech256 1shot 2-1-all" --diffaug-dir=/data/puma_envs/control_augmented_images_caltech256_512  --variations 2 --repeats  --name "exp" --recipe "sgd-pretrain-fullaug"  --repeats 2 --variations 10 --diffaug-fewshot=1 
 
 
-CUDA_VISIBLE_DEVICES=4 python3 train.py /data/torch/caltech256/caltech256 --dataset torch/caltech256  --model=resnet50  --num-classes=257  --log-wandb --experiment "caltech256 1shot 2-1-all" --diffaug-dir=/data/puma_envs/control_augmented_images_caltech256_512  --variations 2 --repeats  --name "base" --recipe "sgd-pretrain-fullaug"  --repeats 22  --diffaug-fewshot=1 --classes 
+CUDA_VISIBLE_DEVICES=4 python3 train.py /data/torch/caltech256/caltech256 --dataset torch/caltech256  --model=resnet50  --num-classes=257  --log-wandb --experiment "caltech256 1shot 2-1-all" --diffaug-dir=/data/puma_envs/control_augmented_images_caltech256_512  --variations 2 --repeats  --name "base" --recipe "sgd-pretrain-fullaug"  --repeats 22  --diffaug-fewshot=1  
