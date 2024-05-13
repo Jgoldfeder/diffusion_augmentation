fc3491192ebf3000db2be403d139cf1beb8938f4
judah <ygoldfed@gmail.com>
2024-05-12 20:23:38 -0400

    dogs

--- pool.py
+++ pool.py
@@ -260,6 +260,56 @@ def get_fewshot_commands_dogs_scratch():
                         commands.append([model,base_name,experiment,recipe,shot,0,way_str,base_repeats,seed])
     return commands
 
+
+
+
+def get_fewshot_commands_dogs_pretrain_sunswitch():
+    commands = []
+
+
+    # define the dataset. Make sure this matches up with the directories given in the machines
+    dataset = "dogs"    
+    
+    # define the sweep to do
+    recipe = "sgd-pretrain-fullaug" 
+    seeds = [10,20,30]    
+    ways = ["5","10"]
+    shots = [1,2,5,10]
+    variations = [15]#,5,10,15]
+    models= ["resnet50sun"]
+    
+    for model in models:
+        for seed in seeds:
+            for way in ways:
+                if way=="all":
+                    way_str = ""
+                if way=="5":
+                    way_str = " --classes 10 23 110 12 40 "
+                if way=="10":
+                    way_str= " --classes  10 23 110 12 40 41 100 96 95 94 "
+                for shot in shots:
+                    for variation in variations:            
+                        experiment = dataset + "-" + way + "-" + str(shot) + "-" + str(variation) + "-pretrainsun"
+
+                        target_repeats = 128
+                        if way=="all":
+                            target_repeats = 16                        
+                        exp_name = "exp seed "+str(seed) + model + " " + recipe
+                        exp_repeats = target_repeats//(variation+1)
+                        
+                        base_name = "base seed"+str(seed) + model + " " + recipe
+                        base_repeats = exp_repeats * (variation+1)
+
+                        commands.append([model,exp_name,experiment,recipe,shot,variation,way_str+ " --switch ",exp_repeats,seed])
+                        commands.append([model,base_name,experiment,recipe,shot,0,way_str,base_repeats,seed])
+    return commands
+
+
+
+
+
+
+
 def get_command_string(command,gpu,aug_directory,data_dir):
     def get_command(device,aug_directory,model,name,experiment,recipe,shots,variations,ways,repeats,seed,data_directory):
         dataset = experiment.split("-")[0]
@@ -286,7 +336,8 @@ def foo(command):
 
 pool = Pool(processes=num_processes)
 
-commands = get_fewshot_commands_dogs_scratch()#get_fewshot_commands_aircraft_pretrain()#get_fewshot_commands_caltech_scratch() + get_fewshot_commands_caltech_pretrain()
+commands = get_fewshot_commands_dogs_pretrain_sunswitch()
+#get_fewshot_commands_dogs_scratch()#get_fewshot_commands_aircraft_pretrain()#get_fewshot_commands_caltech_scratch() + get_fewshot_commands_caltech_pretrain()
 for c in commands:
     print(get_command_string(c,0,machine_1.aug_dir,machine_1.data_dir))
     print(len(commands))
