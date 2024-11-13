6b18952e224e34941ccd2c0d9dcaf1ae10ee8aec
Vaibhav Sourirajan <vs2787@columbia.edu>
2024-11-13 16:54:39 -0500

    initial experiment for fewshot on caltech 256

--- fewshot_dataset.py
+++ fewshot_dataset.py
@@ -1,156 +1,234 @@
 import os
-import torch
-from torch.utils.data import Dataset
+import glob
 from PIL import Image
+import torch
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.nn as nn
+import torchvision.models as models
 
 
-# for one shot, specify whether we can use the nth image in the list
-# specify the number of variations include variations
-class FewShotDataset(Dataset):
-    def __init__(self, root_dir, num_unique_files=0, num_variations=0, transform=None, classes = None, num_repeats = 1, start_idx = 0):
-        """
-        Args:
-            root_dir (string): Directory with all the images.
-            num_unique_files (int): Number of unique files to include in the dataset.
-            include_variations (bool): Whether to include variations or just the original images.
-            transform (callable, optional): Optional transform to be applied on a sample.
-        """
+class FewShotDatasetAugmented(Dataset):
+    def __init__(self, root_dir, dataset, categories, transform=None):
         self.root_dir = root_dir
+        self.dataset = dataset
+        self.categories = categories
         self.transform = transform
-        self.image_paths = []
-        self.labels = []
+        self.data_paths, self.labels = self._create_data_paths()
 
-        for class_id in sorted(os.listdir(root_dir)):  # Ensure class order is consistent
-            if not classes is None:
-                if int(class_id) not in classes:
-                    continue
-            class_path = os.path.join(root_dir, class_id)
-            originals = [fname for fname in os.listdir(class_path) if fname.endswith("_original.png")]
-            # Sort to ensure consistency in selection
-            originals.sort()
+    def _create_data_paths(self):
+        data_paths = []
+        labels = []
+        
+        # Loop through each category, assigning a label for each
+        for label, category in enumerate(self.categories):
+            # Add normal images as individual entries
+            normal_images = glob.glob(os.path.join(self.root_dir, "normal_images", self.dataset, category, "*.jpg"))
+            for normal_img_path in normal_images:
+                data_paths.append(normal_img_path)
+                labels.append(label)
+                
+                # Add each augmentation as an individual entry
+                img_id = normal_img_path.split("/")[-1].split(".")[0]
+                for aug_type in ["Canny", "color_controlnet", "Depth", "Segmentation", "zero123"]:
+                    augmented_img_path = os.path.join(self.root_dir, "augmented_images", self.dataset, aug_type, category, img_id + ".jpg")
+                    data_paths.append(augmented_img_path)
+                    labels.append(label)
+                    
+        return data_paths, labels
 
-            # Start at nth image
-            originals = originals[start_idx:]
+    def __len__(self):
+        return len(self.data_paths)
 
-            # Select the first n unique files
-            selected_originals = originals[:num_unique_files] if num_unique_files > 0 else originals
+    def __getitem__(self, idx):
+        # Retrieve the image path and label
+        img_path = self.data_paths[idx]
+        label = self.labels[idx]
 
-            # Build a base name list from selected originals to ensure matching variations are found
-            base_names = ["_".join(fname.split("_")[:-1]) for fname in selected_originals]
-            print(base_names)
-            for base_name in base_names:
-                # Always include the original image
-                original_path = os.path.join(class_path, f"{base_name}_original.png")
-                for _ in range(num_repeats):
-                    self.image_paths.append(original_path)
-                    self.labels.append(class_id)
+        # Load and transform the image
+        image = Image.open(img_path).convert("RGB")
+        if self.transform:
+            image = self.transform(image)
 
-                if num_variations > 0:
-                    # Include any variations of the selected originals
-                    # create a list of variation numbers to include
-                    variations = [str(i) for i in range(num_variations)]
-                    for variation in variations:
-                        variation_fname = f"{base_name}_{variation}.png"
-                        if variation_fname in os.listdir(class_path):
-                            for _ in range(num_repeats):
-                                self.image_paths.append(os.path.join(class_path, variation_fname))
-                                self.labels.append(class_id)
+        return image, label
+
+class FewShotDatasetNormal(Dataset):
+    def __init__(self, root_dir, dataset, categories, transform=None):
+        self.root_dir = root_dir
+        self.dataset = dataset
+        self.categories = categories
+        self.transform = transform
+        self.data_paths, self.labels = self._create_data_paths_and_labels()
+
+    def _create_data_paths_and_labels(self):
+        data_paths = []
+        labels = []
+        # Loop through each category, gathering paths and labels
+        for label, category in enumerate(self.categories):
+            normal_images = glob.glob(os.path.join(self.root_dir, "normal_images", self.dataset, category, "*.jpg"))
+            data_paths.extend(normal_images)
+            labels.extend([label] * len(normal_images))  # Assign label for each image in the category
+        return data_paths, labels
+
+    def __len__(self):
+        return len(self.data_paths)
+
+    def __getitem__(self, idx):
+        normal_img_path = self.data_paths[idx]
+        label = self.labels[idx]
+
+        # Load the normal image
+        normal_image = Image.open(normal_img_path).convert("RGB")
+        if self.transform:
+            normal_image = self.transform(normal_image)
+
+        return normal_image, label
+
+
+class TestDataset(Dataset):
+    def __init__(self, root_dir, dataset, categories, transform=None):
+        self.root_dir = root_dir
+        self.dataset = dataset
+        self.categories = categories
+        self.transform = transform
+        self.image_paths, self.labels = self._create_test_data()
+
+    def _create_test_data(self):
+        image_paths = []
+        labels = []
+        for idx, category in enumerate(self.categories):
+            category_images = glob.glob(os.path.join(self.root_dir, "torch", self.dataset, "256_ObjectCategories", category, "*.jpg"))
+            image_paths.extend(category_images)
+            labels.extend([idx] * len(category_images))
+        return image_paths, labels
 
     def __len__(self):
         return len(self.image_paths)
 
     def __getitem__(self, idx):
         img_path = self.image_paths[idx]
-        image = Image.open(img_path).convert("RGB")
-        # print(img_path)
-        label = int(self.labels[idx])  # Assuming class_id can be directly converted
-
-        if self.transform:
-            image = self.transform(image)
-
-        return image, label
-    
-
-class ImageDatasetWithFilename(Dataset):
-    def __init__(self, root_dir, transform=None):
-        """
-        Args:
-            root_dir (string): Directory with all the subdirectories containing images.
-            transform (callable, optional): Optional transform to be applied on a sample.
-        """
-        self.root_dir = root_dir
-        self.transform = transform
-        self.classes = []
-        self.samples = self._load_samples()
+        label = self.labels[idx]
         
-
-    def _load_samples(self):
-        """
-        Load all the (image_path, label, image_name) tuples into a list.
-        """
-        samples = []
-        for i, label in enumerate(sorted(os.listdir(self.root_dir))):
-            class_path = os.path.join(self.root_dir, label)
-            if not os.path.isdir(class_path):
-                continue
-            for img_name in os.listdir(class_path):
-                img_path = os.path.join(class_path, img_name)
-                # samples.append((img_path, int(label), img_name))
-                # self.classes.append(label)
-                # Need this for caltech256!!
-                samples.append((img_path, i, img_name))
-                self.classes.append(label)
-        return samples
-
-    def __len__(self):
-        return len(self.samples)
-
-    def __getitem__(self, idx):
-        img_path, label, img_name = self.samples[idx]
-        image = Image.open(img_path).convert('RGB')
-
+        image = Image.open(img_path).convert("RGB")
         if self.transform:
             image = self.transform(image)
+        
+        return image, label
 
-        return image, label, img_name
 
-# Check if the two datasets have the same original images
-if __name__ == "__main__":
-    no_variations = FewShotDataset(root_dir="./control_augmented_images_sun397_512", num_unique_files=5, num_variations=2, start_idx=1)
-    #print the number of classes and images
-    print(len(no_variations))
-    #display the image
-    print(no_variations[0])
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
 
-    variations = FewShotDataset(root_dir="./control_augmented_images_sun397_512", num_unique_files=5, num_variations=2)
-    #print the number of classes and images
-    print(len(variations))
-    print(variations[0])
+categories = ["001.ak47", "002.american-flag", "003.backpack", "004.baseball-bat", "005-baseball-glove"]
 
-    # check if the start index is working with our existing datasets
+dataset_normal = FewShotDatasetNormal(root_dir=".", dataset="caltech256", categories=categories, transform=transform)
+dataset_augmented = FewShotDatasetAugmented(root_dir=".", dataset="caltech256", categories=categories, transform=transform)
+
+train_loader_normal = DataLoader(dataset_normal, batch_size=1, shuffle=True)
+train_loader_augmented = DataLoader(dataset_augmented, batch_size=1, shuffle=True)
+
+model_normal = models.resnet18(pretrained=True)
+model_augmented = models.resnet18(pretrained=True)
+
+num_ftrs = model_normal.fc.in_features
+model_normal.fc = nn.Linear(num_ftrs, len(categories))  # Output layer matches number of categories
+model_augmented.fc = nn.Linear(num_ftrs, len(categories))  # Output layer matches number of categories
+
+
+model_normal = model_normal.to('cuda:0' if torch.cuda.is_available() else 'cpu')
+model_augmented = model_augmented.to('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+
+optimizer_normal = optim.Adam(model_normal.parameters(), lr=1e-4)
+optimizer_augmented = optim.Adam(model_augmented.parameters(), lr=1e-4)
+criterion = nn.CrossEntropyLoss()
+
+# #normal training loop
+# for epoch in range(600):
+#     model_augmented.train()
+#     running_loss = 0.0
+#     for images, labels in train_loader_normal:
+#         images = images.to('cuda:0' if torch.cuda.is_available() else 'cpu')
+#         labels = labels.to('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+#         optimizer_normal.zero_grad()
+        
+#         outputs = model_normal(images)
+#         loss = criterion(outputs, labels)
+#         loss.backward()
+#         optimizer_normal.step()
+        
+#         running_loss += loss.item()
+#     print(f"Epoch {epoch+1}, Loss: {running_loss/len(train_loader_normal)}")
+
+
+# # Test dataset and DataLoader
+# test_dataset = TestDataset(root_dir=".", dataset="caltech256", categories=categories, transform=transform)
+# test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+# # Testing loop
+# model_normal.eval()
+# correct = 0
+# total = 0
+
+# with torch.no_grad():
+#     for images, labels in test_loader:
+#         images, labels = images.to('cuda:0' if torch.cuda.is_available() else 'cpu'), labels.to('cuda:0' if torch.cuda.is_available() else 'cpu')
+#         outputs = model_normal(images)
+#         _, predicted = torch.max(outputs, 1)
+#         total += labels.size(0)
+#         correct += (predicted == labels).sum().item()
+
+# print(f"Test Accuracy: {100 * correct / total}%")
+
+
+#augmented training loop
+for epoch in range(100):
+    model_augmented.train()
+    running_loss = 0.0
+    for images, labels in train_loader_augmented:
+        # Move data to the appropriate device
+        images = images.to('cuda:0' if torch.cuda.is_available() else 'cpu')
+        labels = labels.to('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+        # Zero the parameter gradients
+        optimizer_augmented.zero_grad()
+
+        # Forward pass
+        outputs = model_augmented(images)
+        loss = criterion(outputs, labels)
+        
+        # Backward pass and optimization
+        loss.backward()
+        optimizer_augmented.step()
+        
+        # Accumulate the loss
+        running_loss += loss.item()
     
+    # Print average loss for the epoch
+    print(f"Epoch {epoch+1}, Loss: {running_loss / len(train_loader_augmented)}")
 
-    # Assuming you've already initialized `no_variations` and `variations` datasets
 
-    # Extract img_paths from both datasets
-    no_variations_paths = [no_variations[i][2] for i in range(len(no_variations))]
-    variations_paths = [variations[i][2] for i in range(len(variations))]
+# Test dataset and DataLoader
+test_dataset = TestDataset(root_dir=".", dataset="caltech256", categories=categories, transform=transform)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
-    # Extract just the original image paths
-    no_variations_originals = set([path for path in no_variations_paths if path.endswith("_original.png")])
-    variations_originals = set([path for path in variations_paths if path.endswith("_original.png")])
+# Testing loop
+model_augmented.eval()
+correct = 0
+total = 0
 
-    # sort the originals list
-    no_variations_originals = sorted(no_variations_originals)
-    variations_originals = sorted(variations_originals)
+with torch.no_grad():
+    for images, labels in test_loader:
+        images, labels = images.to('cuda:0' if torch.cuda.is_available() else 'cpu'), labels.to('cuda:0' if torch.cuda.is_available() else 'cpu')
+        outputs = model_augmented(images)
+        _, predicted = torch.max(outputs, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
 
-    # loop through them and check if each r the same
-    for i in range(len(no_variations_originals)):
-        if no_variations_originals[i] != variations_originals[i]:
-            print("not the same")
-            break
-    # Compare
-    # Results
-    print(f"Original images in variations: {len(variations_originals)}")
-    print(f"Original images in no_variations: {len(no_variations_originals)}")
-
+print(f"Test Accuracy: {100 * correct / total}%")
---
+++ fewshot_dataset_backup.py
@@ -0,0 +1,156 @@
+import os
+import torch
+from torch.utils.data import Dataset
+from PIL import Image
+
+
+# for one shot, specify whether we can use the nth image in the list
+# specify the number of variations include variations
+class FewShotDataset(Dataset):
+    def __init__(self, root_dir, num_unique_files=0, num_variations=0, transform=None, classes = None, num_repeats = 1, start_idx = 0):
+        """
+        Args:
+            root_dir (string): Directory with all the images.
+            num_unique_files (int): Number of unique files to include in the dataset.
+            include_variations (bool): Whether to include variations or just the original images.
+            transform (callable, optional): Optional transform to be applied on a sample.
+        """
+        self.root_dir = root_dir
+        self.transform = transform
+        self.image_paths = []
+        self.labels = []
+
+        for class_id in sorted(os.listdir(root_dir)):  # Ensure class order is consistent
+            if not classes is None:
+                if int(class_id) not in classes:
+                    continue
+            class_path = os.path.join(root_dir, class_id)
+            originals = [fname for fname in os.listdir(class_path) if fname.endswith("_original.png")]
+            # Sort to ensure consistency in selection
+            originals.sort()
+
+            # Start at nth image
+            originals = originals[start_idx:]
+
+            # Select the first n unique files
+            selected_originals = originals[:num_unique_files] if num_unique_files > 0 else originals
+
+            # Build a base name list from selected originals to ensure matching variations are found
+            base_names = ["_".join(fname.split("_")[:-1]) for fname in selected_originals]
+            print(base_names)
+            for base_name in base_names:
+                # Always include the original image
+                original_path = os.path.join(class_path, f"{base_name}_original.png")
+                for _ in range(num_repeats):
+                    self.image_paths.append(original_path)
+                    self.labels.append(class_id)
+
+                if num_variations > 0:
+                    # Include any variations of the selected originals
+                    # create a list of variation numbers to include
+                    variations = [str(i) for i in range(num_variations)]
+                    for variation in variations:
+                        variation_fname = f"{base_name}_{variation}.png"
+                        if variation_fname in os.listdir(class_path):
+                            for _ in range(num_repeats):
+                                self.image_paths.append(os.path.join(class_path, variation_fname))
+                                self.labels.append(class_id)
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, idx):
+        img_path = self.image_paths[idx]
+        image = Image.open(img_path).convert("RGB")
+        # print(img_path)
+        label = int(self.labels[idx])  # Assuming class_id can be directly converted
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label
+    
+
+class ImageDatasetWithFilename(Dataset):
+    def __init__(self, root_dir, transform=None):
+        """
+        Args:
+            root_dir (string): Directory with all the subdirectories containing images.
+            transform (callable, optional): Optional transform to be applied on a sample.
+        """
+        self.root_dir = root_dir
+        self.transform = transform
+        self.classes = []
+        self.samples = self._load_samples()
+        
+
+    def _load_samples(self):
+        """
+        Load all the (image_path, label, image_name) tuples into a list.
+        """
+        samples = []
+        for i, label in enumerate(sorted(os.listdir(self.root_dir))):
+            class_path = os.path.join(self.root_dir, label)
+            if not os.path.isdir(class_path):
+                continue
+            for img_name in os.listdir(class_path):
+                img_path = os.path.join(class_path, img_name)
+                # samples.append((img_path, int(label), img_name))
+                # self.classes.append(label)
+                # Need this for caltech256!!
+                samples.append((img_path, i, img_name))
+                self.classes.append(label)
+        return samples
+
+    def __len__(self):
+        return len(self.samples)
+
+    def __getitem__(self, idx):
+        img_path, label, img_name = self.samples[idx]
+        image = Image.open(img_path).convert('RGB')
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, label, img_name
+
+# Check if the two datasets have the same original images
+if __name__ == "__main__":
+    no_variations = FewShotDataset(root_dir="./control_augmented_images_sun397_512", num_unique_files=5, num_variations=2, start_idx=1)
+    #print the number of classes and images
+    print(len(no_variations))
+    #display the image
+    print(no_variations[0])
+
+    variations = FewShotDataset(root_dir="./control_augmented_images_sun397_512", num_unique_files=5, num_variations=2)
+    #print the number of classes and images
+    print(len(variations))
+    print(variations[0])
+
+    # check if the start index is working with our existing datasets
+    
+
+    # Assuming you've already initialized `no_variations` and `variations` datasets
+
+    # Extract img_paths from both datasets
+    no_variations_paths = [no_variations[i][2] for i in range(len(no_variations))]
+    variations_paths = [variations[i][2] for i in range(len(variations))]
+
+    # Extract just the original image paths
+    no_variations_originals = set([path for path in no_variations_paths if path.endswith("_original.png")])
+    variations_originals = set([path for path in variations_paths if path.endswith("_original.png")])
+
+    # sort the originals list
+    no_variations_originals = sorted(no_variations_originals)
+    variations_originals = sorted(variations_originals)
+
+    # loop through them and check if each r the same
+    for i in range(len(no_variations_originals)):
+        if no_variations_originals[i] != variations_originals[i]:
+            print("not the same")
+            break
+    # Compare
+    # Results
+    print(f"Original images in variations: {len(variations_originals)}")
+    print(f"Original images in no_variations: {len(no_variations_originals)}")
+
