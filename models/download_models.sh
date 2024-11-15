a948755c1122aa017b5b4936078049fc8c94f55d
Shreyes Kaliyur <slk2191@columbia.edu>
2024-11-15 15:25:36 -0500

    download models script

---
+++ models/download_models.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# NOTE make sure inside the models folder when running
+
+# Download files for Control Net
+echo "Downloading Control Net files..."
+wget https://huggingface.co/stable-diffusion-v1-5/stable-diffusion-v1-5/resolve/main/v1-5-pruned.ckpt -O v1-5-pruned.ckpt
+wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_canny.pth -O control_v11p_sd15_canny.pth
+wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11f1p_sd15_depth.pth -O control_v11f1p_sd15_depth.pth
+wget https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11p_sd15_seg.pth -O control_v11p_sd15_seg.pth
+
+# Download files for Color Control Net
+echo "Downloading Color Control Net files..."
+wget https://huggingface.co/spaces/abhishek/StableSAM/resolve/main/sam_vit_h_4b8939.pth -O sam_vit_h_4b8939.pth
+wget --no-check-certificate "https://drive.usercontent.google.com/download?id=10r_u7nSi2v5yQR1_EyeN4X79JmeLxTAZ&export=download&confirm=t&uuid=79430cd7-b609-4ba6-a778-fec750cafbd0" -O color_img2img_palette.pt
+wget https://huggingface.co/lllyasviel/Annotators/resolve/main/sk_model.pth -O sk_model.pth
+wget https://huggingface.co/lllyasviel/Annotators/resolve/main/sk_model2.pth -O sk_model2.pth
+
+# Download files for Zero123
+echo "Downloading Zero123 files..."
+wget https://huggingface.co/spaces/cvlab/zero123-live/resolve/d7776c37857ae042bf9e31f74d54f736a432ed8a/105000.ckpt -O zero123_105000.ckpt
+
+echo "All files downloaded successfully."
