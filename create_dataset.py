99c662fd6c7dff0b721f26d2bd0bcd36f5ed924f
Shreyes Kaliyur <slk2191@columbia.edu>
2025-01-20 16:28:12 -0500

    more code

--- create_dataset.py
+++ create_dataset.py
@@ -1,6 +1,56 @@
+import random
+from collections import defaultdict
+
 from torchvision.datasets import Caltech256
+from torch.utils.data import Dataset, Subset
+
+class RemappedDataset(Dataset):
+    def __init__(self, dataset, old_to_new_labels):
+        self.dataset = dataset
+        self.old_to_new_labels = old_to_new_labels
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, index):
+        img, label = self.dataset[index]
+        return img.convert('RGB'), self.old_to_new_labels[label]
+
+def get_label_remapping(old_labels_set):
+    old_to_new_labels = dict()
+    old_labels_sorted = sorted(list(set(old_labels_set)))
+    for i in range(len(old_labels_sorted)):
+        old_to_new_labels[old_labels_sorted[i]] = i
+    return old_to_new_labels
+
+def split_train_test(dataset, class_to_label, labels, num_ways, num_shots, seed):
+    random.seed(seed)
+    target_classes = random.sample(class_to_label.keys(), num_ways)
+    print("[LOG] Selected classes: ", target_classes)
+    target_labels = [class_to_label[target_class] for target_class in target_classes]
+
+    label_to_indexes = defaultdict(list)
+    for index, label in enumerate(labels):
+        if label in target_labels:
+            label_to_indexes[label].append(index)
+
+    train_indexes = []
+    test_indexes = []
+    for label, indexes in label_to_indexes.items():
+        random.shuffle(indexes)
+        train_indexes.extend(indexes[:num_shots])
+        test_indexes.extend(indexes[num_shots:])
+
+    old_to_new_labels = get_label_remapping(set(target_labels))
+    train_dataset = RemappedDataset(Subset(dataset, train_indexes), old_to_new_labels)
+    test_dataset = RemappedDataset(Subset(dataset, test_indexes), old_to_new_labels)
+
+    print(len(train_dataset), len(test_dataset))
+
+    return train_dataset, test_dataset, old_to_new_labels
 
 if __name__ == '__main__':
+	print('main func called')
 	# read in caltech 256 dataset
 
 	dataset_name = 'caltech256'
@@ -18,6 +68,26 @@ if __name__ == '__main__':
 		class_name = parts[1]
 		class_to_label[class_name] = label
 		label_to_class[label] = class_name
-		labels = [label for _, label in dataset]
-	
+	labels = [label for _, label in dataset]
+
+	train_dataset, test_dataset, old_to_new_labels = split_train_test(dataset, class_to_label, labels, num_ways, num_shots, seed=seed)
+
 	breakpoint()
+
+def save_to_dir(dataset, dataset_name, num_shots, seed, train=True):
+    file_path = os.path.join('few_shot_datasets', 
+                            dataset_name, 
+                            num_shots, 
+                            seed, 
+                            'train' if train else 'test')
+    
+    os.makedirs(file_path, exist_ok=True)
+
+    #dataset is a list of tuples (image, label, class_name)
+    for img, label, class_name in dataset:
+        folder_name = f"{label}_{class_name}" #folder name is {label}_{class_name}
+        os.makedirs(os.path.join(file_path, folder_name), exist_ok=True)
+
+        #use number of images in folder to name the image
+        img_count = len(os.listdir(os.path.join(file_path, folder_name)))
+        img.save(os.path.join(file_path, folder_name, f"{img_count}.png"))
