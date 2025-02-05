54a51c914bb43c7cb4d9b8b066a66a3095c50d32
Shreyes Kaliyur <slk2191@columbia.edu>
2025-02-05 17:46:06 -0500

    use new subset naming

--- create_fewshot_datasets.sh
+++ create_fewshot_datasets.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 # Create datasets for Caltech256
-for seed in {41..50}; do
-    for shot in 5 10; do
-        echo "Creating dataset for Caltech256 with seed $seed and shot $shot"
-        python create_dataset.py --dataset caltech256 --seed $seed --num_ways 5 --num_shots $shot
-    done
+
+for subset in {41..50}; do
+    python create_dataset.py --dataset flowers102 --subset $subset --num_ways 5 --num_shots 2
 done
 
-# Create datasets for Flowers102
-for seed in {41..50}; do
-    for shot in 5 10; do
-        echo "Creating dataset for Flowers102 with seed $seed and shot $shot"
-        python create_dataset.py --dataset flowers102 --seed $seed --num_ways 5 --num_shots $shot
-    done
-done
+# for seed in {41..50}; do
+#     for shot in 5 10; do
+#         echo "Creating dataset for Caltech256 with seed $seed and shot $shot"
+#         python create_dataset.py --dataset caltech256 --seed $seed --num_ways 5 --num_shots $shot
+#     done
+# done
+
+# # Create datasets for Flowers102
+# for seed in {41..50}; do
+#     for shot in 5 10; do
+#         echo "Creating dataset for Flowers102 with seed $seed and shot $shot"
+#         python create_dataset.py --dataset flowers102 --seed $seed --num_ways 5 --num_shots $shot
+#     done
+# done
