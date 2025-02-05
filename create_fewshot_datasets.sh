59968b37dadf12cabc2fa4a30f8ff0cecadf40c7
Shreyes Kaliyur <slk2191@columbia.edu>
2025-02-05 18:08:07 -0500

    prep for caltech

--- create_fewshot_datasets.sh
+++ create_fewshot_datasets.sh
@@ -5,6 +5,10 @@ for subset in {41..50}; do
     python create_dataset.py --dataset flowers102 --subset $subset --num_ways 5 --num_shots 2
 done
 
+for subset in {41..50}; do
+    python create_dataset.py --dataset flowers102 --subset $subset --num_ways 5 --num_shots 2
+done
+
 # for seed in {41..50}; do
 #     for shot in 5 10; do
 #         echo "Creating dataset for Caltech256 with seed $seed and shot $shot"
