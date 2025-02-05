cb5f724ce2cbabd594621a09e4ab5f692af93d5d
Shreyes Kaliyur <slk2191@columbia.edu>
2025-02-05 17:50:35 -0500

    use subset naming

--- test_random_trees.sh
+++ test_random_trees.sh
@@ -3,25 +3,25 @@
 # Datasets to test
 
 # Number of shots to test
-shots=(5 10)
+shots=(2)
 
-flowers102_seeds=(47 48 50)
-caltech_seeds=(42 43 48)
+flowers102_subsets=(47 48 50)
+caltech_subsets=(42 43 48)
+
+# for shot in "${shots[@]}"; do
+#     for subset in "${caltech_subsets[@]}"; do
+#         for run in {1..3}; do
+#             echo "Running test for caltech256, ${shot}-shot, subset $subset, run $run"
+#             python test_random_tree.py --dataset caltech256 --subset $subset --num_ways 5 --num_shots $shot
+#         done
+#     done
+# done
 
 for shot in "${shots[@]}"; do
-    for seed in "${caltech_seeds[@]}"; do
+    for subset in "${flowers102_subsets[@]}"; do
         for run in {1..3}; do
-            echo "Running test for caltech256, ${shot}-shot, seed $seed, run $run"
-            python test_random_tree.py --dataset caltech256 --seed $seed --num_ways 5 --num_shots $shot
-        done
-    done
-done
-
-for shot in "${shots[@]}"; do
-    for seed in "${flowers102_seeds[@]}"; do
-        for run in {1..3}; do
-            echo "Running test for flowers102, ${shot}-shot, seed $seed, run $run"
-            python test_random_tree.py --dataset flowers102 --seed $seed --num_ways 5 --num_shots $shot
+            echo "Running test for flowers102, ${shot}-shot, subset $subset, run $run"
+            python test_random_tree.py --dataset flowers102 --subset $subset --num_ways 5 --num_shots $shot
         done
     done
 done
