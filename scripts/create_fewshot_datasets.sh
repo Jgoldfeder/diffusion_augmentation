e0490f0a7446942f46255421c2019bcb5f71da3f
Shreyes Kaliyur <slk2191@columbia.edu>
2025-02-12 16:21:30 -0500

    change to use dataset manager

--- scripts/create_fewshot_datasets.sh
+++ scripts/create_fewshot_datasets.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 # Create datasets for Caltech256
 
-# for subset in {41..50}; do
-#     python create_dataset.py --dataset flowers102 --subset $subset --num_ways 5 --num_shots 2
-# done
-
 for subset in {41..50}; do
-    python create_dataset.py --dataset caltech256 --subset $subset --num_ways 5 --num_shots 2
+    python dataset_manager.py --dataset flowers102 --subset $subset --num_ways 5 --num_shots 2
 done
 
-# for seed in {41..50}; do
+# for subset in {41..50}; do
+#     python dataset_manager.py --dataset caltech256 --subset $subset --num_ways 5 --num_shots 2
+# done
+
+# for subset in {41..50}; do
 #     for shot in 5 10; do
-#         echo "Creating dataset for Caltech256 with seed $seed and shot $shot"
-#         python create_dataset.py --dataset caltech256 --seed $seed --num_ways 5 --num_shots $shot
+#         echo "Creating dataset for Caltech256 with subset $subset and shot $shot"
+#         python dataset_manager.py --dataset caltech256 --subset $subset --num_ways 5 --num_shots $shot
 #     done
 # done
 
 # # Create datasets for Flowers102
-# for seed in {41..50}; do
+# for subset in {41..50}; do
 #     for shot in 5 10; do
-#         echo "Creating dataset for Flowers102 with seed $seed and shot $shot"
-#         python create_dataset.py --dataset flowers102 --seed $seed --num_ways 5 --num_shots $shot
+#         echo "Creating dataset for Flowers102 with subset $subset and shot $shot"
+#         python dataset_manager.py --dataset flowers102 --subset $subset --num_ways 5 --num_shots $shot
 #     done
 # done
