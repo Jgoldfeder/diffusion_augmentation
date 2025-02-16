848ffdc5da64275ed27e196c20159af7991d9717
Vaibhav Sourirajan <vs2787@columbia.edu>
2025-02-16 15:14:30 -0500

    stanford dogs

--- dataset_manager.py
+++ dataset_manager.py
@@ -34,6 +34,20 @@ def get_dataset_from_torch(dataset_name: str, root='./torch') -> tuple[Dataset, 
 		label_to_class = dict()
 		for label_str, class_name in label_str_to_class.items():
 			label_to_class[int(label_str)] = class_name
+	elif dataset_name == 'stanford_dogs':
+		data_dir = os.path.join(root, 'stanford_dogs', 'Images')
+		label_to_class = dict()
+		label = 0
+		for category in os.listdir(data_dir):
+			if category == '.DS_Store':
+				continue
+			parts = category.split('-')
+			class_name = parts[1].lower()
+			label_to_class[label] = class_name
+			label += 1
+		print(label_to_class)
+		dataset = ImageFolder(data_dir)
+
 	else:
 		raise Exception(f"Dataset {dataset_name} not supported")
 	return dataset, label_to_class
