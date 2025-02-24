c427149bcae539a002407e6329fc026ee02246c6
Shreyes Kaliyur <slk2191@columbia.edu>
2025-02-24 13:36:57 -0500

    denorm stuff

--- test_random_tree.py
+++ test_random_tree.py
@@ -7,6 +7,7 @@ import pygad
 import logging
 import argparse
 import torch
+from torchvision import transforms
 import numpy as np
 from PIL import Image
 import network_model
@@ -29,12 +30,21 @@ def tree_to_genome(tree):
         if node.right:
             q.append(node.right)
     return genome
+
+# Define the mean and std used in normalization
+mean = torch.tensor([0.485, 0.456, 0.406]).view(3, 1, 1)  # Reshape for broadcasting
+std = torch.tensor([0.229, 0.224, 0.225]).view(3, 1, 1)
+
+def denormalize(tensor):
+    return tensor * std + mean
+
 def main():
 
     random.seed(time.time())
     
     tree = BinaryAugmentationNode()
-    tree.make_random_tree(3)
+    tree.augmentation_type = AugmentationType.CANNY
+    # tree.make_random_tree(3)
     genome = tree_to_genome(tree)
     print(genome)
 
@@ -51,16 +61,20 @@ def main():
     os.makedirs(f"augmented_images/{str(genome)}", exist_ok=True)
     for i, (img, label) in enumerate(dataset):
         #convert the tensor to a PIL Image
+        img = denormalize(img)
         img = torch.clamp(img, 0, 1)  # Ensure values are in [0,1] range
-        img = (img * 255).byte()  # Scale to [0,255] and convert to bytes
-        img = img.permute(1, 2, 0)  # Change from CxHxW to HxWxC format
-        img = Image.fromarray(img.cpu().numpy())
+        to_pil = transforms.ToPILImage()
+        img = to_pil(img)
+        # img = (img * 255).byte()  # Scale to [0,255] and convert to bytes
+        # img = img.permute(1, 2, 0)  # Change from CxHxW to HxWxC format
+        # img = Image.fromarray(img.cpu().numpy())
         img.save(f"augmented_images/{str(genome)}/{i}.jpg")
     train_dataset, val_dataset = dataset_manager.split_train_val(dataset)
 
     model = network_model.get_model_for_finetune(ModelType.RESNET50, num_ways)
-    model_results: ModelResults = network_model.train_and_val(model, train_dataset, val_dataset, num_iterations_for_val = 10, device = "cuda")
+    model_results: ModelResults = network_model.train_and_val(model, train_dataset, val_dataset, num_epochs = 10, device = "cuda")
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
     main()
