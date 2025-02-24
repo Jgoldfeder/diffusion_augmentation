70cf7889fd1149b5d94757d93604cb5d38c4777c
Vaibhav Sourirajan <vs2787@columbia.edu>
2025-02-24 00:11:06 -0500

    random tree test works

--- test_random_tree.py
+++ test_random_tree.py
@@ -1,6 +1,6 @@
 import random
 import os
-os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
 import wandb
 import pygad
@@ -8,7 +8,7 @@ import logging
 import argparse
 import torch
 import numpy as np
-
+from PIL import Image
 import network_model
 import dataset_manager
 from network_model import ModelResults, ModelType
@@ -16,29 +16,27 @@ from dataset_manager import FolderDataset
 from augmentation_tree import BinaryAugmentationNode, AugmentationType, TreeAugmentedDataset, ProbabilityLimits
 import time
 
-def tree_to_string(node, level=0, direction='root'):
-    tree_str = ''
-    if node:
-        if not node.left and not node.right:
-            # Leaf nodes: only show augmentation type
-            edge_info = f"(Augmentation: {node.augmentation_type})"
-        else:
-            # Non-leaf nodes: show augmentation type and probabilities
-            edge_info = f"(Augmentation: {node.augmentation_type}, L_prob: {node.left_probability:.2f}, R_prob: {1-node.left_probability:.2f})"
-        tree_str += '  ' * level + f"{direction}: {edge_info}" + '\n'
+def tree_to_genome(tree):
+    genome = []
+    q = []
+    q.append(tree)
+    while len(q) > 0:
+        node = q.pop(0)
+        genome.append(node.augmentation_type.value)
+        genome.append(round(node.left_probability, 2))
         if node.left:
-            tree_str += tree_to_string(node.left, level + 1, 'L')
+            q.append(node.left)
         if node.right:
-            tree_str += tree_to_string(node.right, level + 1, 'R')
-    return tree_str
-
+            q.append(node.right)
+    return genome
 def main():
 
     random.seed(time.time())
     
     tree = BinaryAugmentationNode()
     tree.make_random_tree(3)
-    print(tree_to_string(tree))
+    genome = tree_to_genome(tree)
+    print(genome)
 
     dataset_name = "flowers102"
     num_ways = 5
@@ -50,12 +48,17 @@ def main():
     dataset = TreeAugmentedDataset(train_path, tree, num_augmentations_per_image = 5)
     #save all the images in the dataset to a folder
     os.makedirs("augmented_images", exist_ok=True)
-    os.makedirs(f"augmented_images/{tree_to_string(tree)}", exist_ok=True)
+    os.makedirs(f"augmented_images/{str(genome)}", exist_ok=True)
     for i, (img, label) in enumerate(dataset):
-        img.save(f"augmented_images/{tree_to_string(tree)}/{i}.jpg")
+        #convert the tensor to a PIL Image
+        img = torch.clamp(img, 0, 1)  # Ensure values are in [0,1] range
+        img = (img * 255).byte()  # Scale to [0,255] and convert to bytes
+        img = img.permute(1, 2, 0)  # Change from CxHxW to HxWxC format
+        img = Image.fromarray(img.cpu().numpy())
+        img.save(f"augmented_images/{str(genome)}/{i}.jpg")
     train_dataset, val_dataset = dataset_manager.split_train_val(dataset)
 
-    model = network_model.get_model_for_finetune(ModelType.RESNET18, num_ways)
+    model = network_model.get_model_for_finetune(ModelType.RESNET50, num_ways)
     model_results: ModelResults = network_model.train_and_val(model, train_dataset, val_dataset, num_iterations_for_val = 10, device = "cuda")
 
 
