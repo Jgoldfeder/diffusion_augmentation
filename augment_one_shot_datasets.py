921a43c1d8920b4f46848a7c8ced57672814812c
Vaibhav Sourirajan <vs2787@columbia.edu>
2025-03-04 19:58:58 -0500

    augment one shot datasets with one classical aug

---
+++ augment_one_shot_datasets.py
@@ -0,0 +1,19 @@
+from dataset_manager import get_classical_transform
+import os
+from PIL import Image
+
+datasets = ["stanford_cars", "flowers102", "caltech256", "stanford_dogs", "food101"]
+
+for dataset in datasets:
+    base_path = os.path.join("few_shot_datasets", dataset, "5_ways", "1_shot")
+    for subset in range(41, 50):
+        train_path = os.path.join(base_path, f"subset_{subset}", "train")
+        for class_name in os.listdir(train_path):
+            full_data_path = os.path.join(train_path, class_name)
+            #load 0.png
+            img = Image.open(os.path.join(full_data_path, "0.png"))
+            transform = get_classical_transform()
+            augmented_img = transform(img)
+            augmented_img.save(os.path.join(full_data_path, "1.png"))
+
+            print(f"Augmented image for {full_data_path}")
