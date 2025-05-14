8f08917fcb663e36f6c6300bbdac7867906a4703
Vaibhav Sourirajan <vs2787@columbia.edu>
2025-05-13 20:59:30 -0400

    create datasets

--- scripts/create_fewshot_datasets.sh
+++ scripts/create_fewshot_datasets.sh
@@ -3,15 +3,45 @@
 
 datasets=("stanford_cars" "flowers102" "stanford_dogs" "caltech256" "food101")
 
+python dataset_manager.py --dataset caltech256 --subset 43 --num_ways 5 --num_shots 1
+python dataset_manager.py --dataset caltech256 --subset 42 --num_ways 5 --num_shots 2
+python dataset_manager.py --dataset caltech256 --subset 42 --num_ways 5 --num_shots 5
+python dataset_manager.py --dataset caltech256 --subset 42 --num_ways 10 --num_shots 2
+python dataset_manager.py --dataset caltech256 --subset 42 --num_ways 10 --num_shots 2
 
+
+python dataset_manager.py --dataset flowers102 --subset 50 --num_ways 5 --num_shots 2
+python dataset_manager.py --dataset flowers102 --subset 50 --num_ways 5 --num_shots 5
+python dataset_manager.py --dataset flowers102 --subset 50 --num_ways 10 --num_shots 2
+python dataset_manager.py --dataset flowers102 --subset 50 --num_ways 10 --num_shots 5
+
+python dataset_manager.py --dataset stanford_dogs --subset 46 --num_ways 5 --num_shots 1
+python dataset_manager.py --dataset stanford_dogs --subset 47 --num_ways 5 --num_shots 2
+python dataset_manager.py --dataset stanford_dogs --subset 47 --num_ways 5 --num_shots 5
+python dataset_manager.py --dataset stanford_dogs --subset 45 --num_ways 10 --num_shots 2
+python dataset_manager.py --dataset stanford_dogs --subset 45 --num_ways 10 --num_shots 5
+
+python dataset_manager.py --dataset stanford_cars --subset 44 --num_ways 5 --num_shots 1
+python dataset_manager.py --dataset stanford_cars --subset 44 --num_ways 5 --num_shots 2
+python dataset_manager.py --dataset stanford_cars --subset 44 --num_ways 5 --num_shots 5
+python dataset_manager.py --dataset stanford_cars --subset 48 --num_ways 10 --num_shots 2
+python dataset_manager.py --dataset stanford_cars --subset 48 --num_ways 10 --num_shots 5
+
+
+python dataset_manager.py --dataset oxford-iiit-pet --subset 44 --num_ways 5 --num_shots 1
+python dataset_manager.py --dataset oxford-iiit-pet --subset 44 --num_ways 5 --num_shots 2
+python dataset_manager.py --dataset oxford-iiit-pet --subset 44 --num_ways 5 --num_shots 5
+python dataset_manager.py --dataset oxford-iiit-pet --subset 45 --num_ways 10 --num_shots 2
+python dataset_manager.py --dataset oxford-iiit-pet --subset 45 --num_ways 10 --num_shots 5
+
+
+python dataset_manager.py --dataset food101 --subset 43 --num_ways 5 --num_shots 1
+python dataset_manager.py --dataset food101 --subset 43 --num_ways 5 --num_shots 2
+python dataset_manager.py --dataset food101 --subset 43 --num_ways 5 --num_shots 5
 python dataset_manager.py --dataset food101 --subset 42 --num_ways 10 --num_shots 2
+python dataset_manager.py --dataset food101 --subset 43 --num_ways 10 --num_shots 5
 
-for dataset in "${datasets[@]}"; do
-    for subset in {41..50}; do
-        echo "Creating dataset for $dataset with subset $subset num_ways 10 num_shots 2"
-        python dataset_manager.py --dataset $dataset --subset $subset --num_ways 10 --num_shots 2
-    done
-done
+
 
 # for subset in {41..50}; do
 #     python dataset_manager.py --dataset flowers102 --subset $subset --num_ways 5 --num_shots 1
